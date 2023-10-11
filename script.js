c24bc9a2b8a65807d6779b7f8f6ed96418773ad2
Gustavo <duartesampaiogustavo@gmail.com>
2023-10-11 11:29:12 -0300

    alterações

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
+
+
+function play () {
+const meuBotao = document.querySelector(".meuBotao");
+meuBotao.style.background = "transparent";
+meuBotao.style.color = "transparent";
+meuBotao.style.border = "none";
+
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const jump1 = document.querySelector(".jump");
+const score = document.querySelector('.score');
+
+let playerScore = "";
+let gameIsOver = false;
+
+  const scoreCounter = () => {
+    if (!gameIsOver) {
+      playerScore++;
+      score.innerHTML = `Score <b>${playerScore}</b>`;
+    }
+  };
+
+  const interval = 300;
+
+  Interval = setInterval(scoreCounter, interval);
+
+const loop = setInterval(() => {
+
+
 
         const jump = () => {
             mario.classList.add('jump');
             setTimeout(() => {
-                mario.classList.remove('jump');
+            mario.classList.remove('jump');
             }, 500);
-        }
+        };
 
-        const loop = setInterval(() => {
+        document.addEventListener('keydown', function(event) {
+            if (event.key === " ") {
+              jump();
+              var audio = document.getElementById('Audio');
+              audio.volume = 0.1;
+              audio.play();
+            }
 
+          });
+            
             const pipePosition = pipe.offsetLeft;
             const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
@@ -28,15 +64,14 @@ const pipe = document.querySelector('.pipe');
                 const gameOver = document.getElementById('game-over');
                 gameOver.style.display = 'flex';
 
+                gameIsOver = true;
                 clearInterval(loop);
+                clearInterval(Interval);
             }
 
         }, 10)
 
-document.addEventListener('keydown', jump);
-
-document.addEventListener('keydown', function () {
-    var audio = document.getElementById('Audio');
-    audio.volume = 0.1;
-    audio.play();
-});
+          pipe.style.animationPlayState = "running";
+          jump1.style.animationPlayState = "running";
+          
+};
